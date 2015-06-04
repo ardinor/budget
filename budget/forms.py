2626d56c69950e24f8f3c0a334307bb74749332b
ardinor <jordan@defestri.org>
2015-06-04 20:53:05 +1000

    This seems like more work than necessary... should just go back to the other, original non-form way of doing it

--- budget/forms.py
+++ budget/forms.py
@@ -1,7 +1,8 @@
 from django.forms import ModelForm
 
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
+from crispy_forms.layout import Submit, Button, Layout, MultiField, Div
+from crispy_forms.bootstrap import FormActions, PrependedText
 
 from budget.models import Account
 
@@ -17,8 +18,29 @@ class NewAccountForm(ModelForm):
         self.helper.form_method = 'post'
         self.helper.form_action = 'budget:account_save'
 
-        # You can dynamically adjust your layout
-        self.helper.layout.append(Submit('save', 'save'))
+        self.helper.layout = Layout(
+            MultiField(
+                '',
+                Div(
+                    'name',
+                    css_id = 'form-group'
+                ),
+                Div(
+                    'bank',
+                    css_id = 'form-group'
+                ),
+                Div(
+                    'acc_type',
+                    css_id = 'form-group'
+                ),
+
+            ),
+            PrependedText('balance', '$'),
+            FormActions(
+                Submit('save', 'Save changes'),
+                Button('cancel', 'Cancel')
+            ),
+        )
 
     class Meta:
         model = Account
