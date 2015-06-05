f2de7251d6839805658820d9252daab158711116
ardinor <jordan@defestri.org>
2015-06-05 19:56:41 +1000

    Fixed missing import

--- budget/views.py
+++ budget/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404, render
+from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 
 from budget.models import Account, Transaction, Reasons, AccountTypes
